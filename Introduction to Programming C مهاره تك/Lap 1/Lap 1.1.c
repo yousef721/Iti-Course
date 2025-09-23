416f1ec0e4b4b63a0e70fdf3eaecfe83c5af6dab
Yousef721 <yousarsefabedalla255@gmail.com>
2025-09-23 07:39:47 +0300

    first commit

---
+++ Introduction to Programming C مهاره تك/Lap 1/Lap 1.1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("I'm Yousef Abdullah \n");
+    printf("My birthday is 10July 2001 \n");
+    printf("I graduated from faculty of physical education, Benha university");
+    return 0;
+}
